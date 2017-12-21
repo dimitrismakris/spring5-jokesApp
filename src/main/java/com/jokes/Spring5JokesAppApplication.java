8b7bfcd8e45f02089aa0fcb0c781d814483102be
Dimitris Makris <dimitrismakris.e@gmail.com>
2017-12-22 00:37:18 +0100

    Initial commit

---
+++ src/main/java/com/jokes/Spring5JokesAppApplication.java
@@ -0,0 +1,12 @@
+package com.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppApplication.class, args);
+	}
+}
